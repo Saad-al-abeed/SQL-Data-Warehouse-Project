da5abf7eaf05551c30ef58c174315b9d9c905d67
Saad Al Abeed <77157656+Saad-al-abeed@users.noreply.github.com>
2026-04-18 20:06:28 +0600

    Add files via upload

---
+++ scripts/gold_checks.sql
@@ -0,0 +1,31 @@
+-- checking 'gold.dim_customers'
+
+select 
+    customer_key,
+    count(*) as duplicate_count
+from gold.dim_customers
+group by customer_key
+having count(*) > 1;
+-- should return nothing
+
+
+-- checking 'gold.dim_products'
+
+select 
+    product_key,
+    count(*) as duplicate_count
+from gold.dim_products
+group by product_key
+having count(*) > 1;
+-- should return nothing
+
+-- checking 'gold.fact_sales'
+
+select * from gold.fact_sales f
+left join gold.dim_customers c
+    on c.customer_key = f.customer_key
+left join gold.dim_products p
+    on p.product_key = f.product_key
+where p.product_key is null 
+   or c.customer_key is null;
+-- should return nothing
